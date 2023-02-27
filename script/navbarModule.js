3ad5de4c484c9ff6726f8b26ca6c885ae0cf9308
radwa nabil <radwaismail444@gmail.com>
2023-02-27 23:40:30 +0200

    navbar fixed

---
+++ script/navbarModule.js
@@ -0,0 +1,64 @@
+import { User } from "./user.js";
+
+const user = document.querySelector("#user");
+const cart = document.querySelector("#cart");
+const userBoxName = document.querySelector('.user-content p');
+const userBox = document.querySelector(".user-container");
+const nav = document.querySelector("nav");
+const logOut = document.querySelector("#logOut");
+
+
+
+///when pressed on user card checks if their is a user logged in 
+user.addEventListener('click', function () {
+
+    if (loggedInUser.isUserLoggedIn()) {
+        let user = loggedInUser.isUserLoggedIn();
+        console.log(user);
+        let firstName = user.firstName;
+        let lastName = user.lastName;
+        userBoxName.innerText = `${firstName} ${lastName}`;
+        if (userBox.style.display === "none") {
+            userBox.style.display = "block";
+
+        } else {
+            userBox.style.display = "none";
+        }
+    } else {
+        location.assign('./log-in.html');
+    }
+
+
+}
+)
+
+//check if thier is a user logged in when pressed on cart
+cart.addEventListener('click', function () {
+    if (loggedInUser.isUserLoggedIn()) {
+        location.assign('./cart.html');
+    } else {
+        location.assign('./log-in.html');
+    }
+})
+
+
+logOut.addEventListener('click', function () {
+
+    var users = localStorage.getItem('users');
+    var userData = JSON.parse(users);
+    userData.forEach(element => {
+        console.log(element);
+        if (element.active === true) {
+            element.active = false;
+        }
+
+    });
+    
+    var json = JSON.stringify(userData);
+    localStorage.setItem('users', json);
+    userBox.style.display = "none"
+
+})
+
+var loggedInUser = new User();
+
