d1ccb88944758e5428f8a03cc348e564846d108d
Nada Saeed <nada1998saeed@gmail.com>
2023-02-28 19:40:44 +0200

    modifing product details

--- script/productDetails.js
+++ script/productDetails.js
@@ -8,6 +8,7 @@ export class ProductDetails {
     #addToCartBtn = null;
     counter = 1;
     constructor(product) {
+        console.log(product)
         this.user = new User();
         this.id = product.id;
         this.images = product.images;
@@ -46,67 +47,68 @@ export class ProductDetails {
     showModalProductDetails() {
         this.createProductDetaisModal();
         var modal = bootstrap.Modal.getOrCreateInstance(
-        detailsModal.querySelector("#myModal")
+          detailsModal.querySelector("#myModal")
         );
+        console.log(modal)
         modal.show();
     }
 
     createProductDetaisModal() {
-        detailsModal.insertAdjacentHTML(
-        "beforeend",
+        detailsModal.innerHTML = 
+        
         `<div class="modal fade modal-lg Details-modal mt-md-5" id="myModal">
-                    <div class="modal-dialog ">
-                        <div class="modal-content p-4 p-lg-5">
-                            <button type="button" class="btn-close Details-model-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            <div class="modal-body row col-12 d-lg-flex justify-content-between p-0 py-4 ">
-                                <div class="col-12 col-md-5 DetailsImage my-auto ml-lg-auto">
-                                    <div class="Image-container DetailsLargeScreen">
-                                        <div class="ActiveDetailsImage">
-                                            <img src="${this.images[0]}" alt="green apple slice">
-                                        </div>
-                                        <div class="DetailsImageAlbum col-12 ">
-                                            <ul class="DetailsImageList p-0 m-0">
-                                                <li class="activeLiImage"><img src="${this.images[0]}" alt="${this.title} image 1"></li>
-                                                <li><img src="${this.images[1]}" alt="${this.title} image 2"></li>
-                                                <li><img src="${this.images[2]}" alt="${this.title} image 3"></li>
-                                                <li><img src="${this.images[3]}" alt="${this.title} image 4"></li>
-                                            </ul>
-                                        </div>
-                                    </div>
+            <div class="modal-dialog ">
+                <div class="modal-content p-4 p-lg-5">
+                    <button type="button" class="btn-close Details-model-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <div class="modal-body row col-12 d-lg-flex justify-content-between p-0 py-4 ">
+                        <div class="col-12 col-md-5 DetailsImage my-auto ml-lg-auto">
+                            <div class="Image-container DetailsLargeScreen">
+                                <div class="ActiveDetailsImage">
+                                    <img src="${this.images[0]}" alt="green apple slice">
                                 </div>
-
-                                <div class="col-12 col-md-7 DetailsText my-auto pt-4 p-md-0 ml-lg-auto">
-                                    <h3 class="fs-3 text-uppercase">${this.title}</h3>
-                                    <div class="ProductDetails-rating pb-2">
-                                    </div>
-                                    <p class="">${this.description}</p>
-                                    <p class="pb-1 m-0"><b>price: </b>$ ${this.price}</p>
-                                    <p class="pb-1 m-0"><b>Brand: </b>${this.brand}</p>
-                                    <p class="pb-1 m-0"><b>Category: </b>${this.category}</p>
-                                    <p class="pb-1 m-0"><b>stock: </b>${(this.stock> 0) ? "In stock" : "out of stock"}</p>
-                                    <div class="productDetailsForm d-flex justify-content-between pt-3">
-                                        <div class="d-flex p-0 col-5 col-md-6 col-lg-4 productQuantityContainer">
-                                            <button class="minusbtn btn btn-link p-0 pe-2">
-                                                <i class="bi bi-dash fs-5"></i>
-                                            </button>
-
-                                            <input id="form1" min="1" name="quantity" value="1" type="number"
-                                                class="quantityBtn form-control form-control-sm" />
-
-                                            <button class="plusbtn btn btn-link p-0 ps-2">
-                                                <i class="bi bi-plus fs-5"></i>
-                                            </button>
-                                        </div>
-                                        <button type="button" class="btn btn-primary col-5 col-md-6 col-lg-4 addToCartBtn"> 
-                                            Add to cart
-                                        </button>
-                                    </div>
+                                <div class="DetailsImageAlbum col-12 ">
+                                    <ul class="DetailsImageList p-0 m-0">
+                                        <li class="activeLiImage"><img src="${this.images[0]}" alt="${this.title} image 1"></li>
+                                        <li><img src="${this.images[1]}" alt="${this.title} image 2"></li>
+                                        <li><img src="${this.images[2]}" alt="${this.title} image 3"></li>
+                                        <li><img src="${this.images[3]}" alt="${this.title} image 4"></li>
+                                    </ul>
                                 </div>
                             </div>
                         </div>
+
+                        <div class="col-12 col-md-7 DetailsText my-auto pt-4 p-md-0 ml-lg-auto">
+                            <h3 class="fs-3 text-uppercase">${this.title}</h3>
+                            <div class="ProductDetails-rating pb-2">
+                            </div>
+                            <p class="">${this.description}</p>
+                            <p class="pb-1 m-0"><b>price: </b>$ ${this.price}</p>
+                            <p class="pb-1 m-0"><b>Brand: </b>${this.brand}</p>
+                            <p class="pb-1 m-0"><b>Category: </b>${this.category}</p>
+                            <p class="pb-1 m-0"><b>stock: </b>${(this.stock> 0) ? "In stock" : "out of stock"}</p>
+                            <div class="productDetailsForm d-flex justify-content-between pt-3">
+                                <div class="d-flex p-0 col-5 col-md-6 col-lg-4 productQuantityContainer">
+                                    <button class="minusbtn btn btn-link p-0 pe-2">
+                                        <i class="bi bi-dash fs-5"></i>
+                                    </button>
+
+                                    <input id="form1" min="1" name="quantity" value="1" type="number"
+                                        class="quantityBtn form-control form-control-sm" />
+
+                                    <button class="plusbtn btn btn-link p-0 ps-2">
+                                        <i class="bi bi-plus fs-5"></i>
+                                    </button>
+                                </div>
+                                <button type="button" class="btn btn-primary col-5 col-md-6 col-lg-4 addToCartBtn"> 
+                                    Add to cart
+                                </button>
+                            </div>
+                        </div>
                     </div>
-                </div>`
-        );
+                </div>
+            </div>
+        </div>`
+        
     }
 
     ratingStars(){
@@ -186,5 +188,4 @@ export class ProductDetails {
             this.user.AddToCart(item);
         }
     }
-
 }
